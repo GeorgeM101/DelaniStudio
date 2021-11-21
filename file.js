ed341b00ef05108cc480f646e33459c9ba7e4959
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-21 22:51:06 +0300

    edited portfolio section

---
+++ file.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#work").hover(function(){
+        $(this).css("background-color", "black")
+    });
+});
+
+
+var names = document.getElementById("name");
+var email = document.getElementById("Email");
+var messages = document.getElementById("mess");
+var results =  document.getElementById("results")
+
+function formInput(){
+    if(name.value === "")
+    console.log("input name")
+};
+
