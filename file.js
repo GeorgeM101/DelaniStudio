50b23d8e378e596579a02913e02ee0a6f670e256
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-22 08:55:32 +0300

    add hidden text second image

--- file.js
+++ file.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
     $(".img1").click(function(){
       $(".img1").hide(100);
-      $(".first").toggle(100);
       $(".first").show(100);
-    })
+    });
     $(".first").click(function(){
-      $(".first").hide(1200);
-      $(".img1").toggle(1200);
-      $(".img1").show(1200);
+      $(".first").hide(100);
+      $(".img1").show(100);
     });
 });
+$(document).ready(function(){
+    $(".img2").click(function(){
+      $(".img2").hide(100);
+      $(".second").show(100);
+    });
+    $(".second").click(function(){
+      $(".second").hide(100);
+      $(".img2").show(100);
+    });
+});
+$(document).ready(function(){
+    $(".img3").click(function(){
+      $(".img3").hide(100);
+      $(".third").show(100);
+    });
+    $(".third").click(function(){
+      $(".third").hide(100);
+      $(".img3").show(100);
+    });
+});
+
+
 
 // $(document).ready(function(){
 //     $('#work').hover(function () {
