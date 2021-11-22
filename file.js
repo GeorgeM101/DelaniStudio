04bbfa094723c5f16758552ba28d9e8c7cfac3ac
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-22 19:25:46 +0300

    added readme

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $(".img1").click(function(){
-      $(".img1").hide(100);
+    $(".img1").onClick(function(){
+      $(".img1").hide(0);
       $(".first").show(100);
     });
     $(".first").click(function(){
@@ -10,43 +10,48 @@ $(document).ready(function(){
 });
 $(document).ready(function(){
     $(".img2").click(function(){
-      $(".img2").hide(100);
+      $(".img2").hide(0);
       $(".second").show(100);
     });
     $(".second").click(function(){
-      $(".second").hide(100);
+      $(".second").hide(0);
       $(".img2").show(100);
     });
 });
 $(document).ready(function(){
     $(".img3").click(function(){
-      $(".img3").hide(100);
+      $(".img3").hide(0);
       $(".third").show(100);
     });
     $(".third").click(function(){
-      $(".third").hide(100);
+      $(".third").hide();
       $(".img3").show(100);
     });
+});
+s
+
+$(document).ready(function(){
+    $('#work').hover(function () {
+    $(this).animate({opacity:'1'});
+    },
+    function () {
+    $(this).animate({opacity:'0'});
+});
 });
 
 
 
-// $(document).ready(function(){
-//     $('#work').hover(function () {
-//     $(this).animate({opacity:'1'});
-//     },
-//     function () {
-//     $(this).animate({opacity:'0'});
-// });
-// });
 
-var names = document.getElementById("name");
-var email = document.getElementById("Email");
-var messages = document.getElementById("mess");
-var results =  document.getElementById("results")
+function validate(){
+  var names = getElementById("name");
+  var emails = getElementById("email");
+  var messages = getElementById("mess");
 
-function formInput(){
-    if(name.value === "")
-    console.log("input name")
-};
+  if(names.value === "" || emails.value === "" || messages.value === "")
+  alert("thank you")
+
+
+
+
+}
 
